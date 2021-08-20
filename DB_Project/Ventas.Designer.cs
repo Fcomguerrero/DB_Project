87719d0b9da4def9be0b7a10419915d0fb992bdd
Francis <Francis@DESKTOP-B7A7GH7>
2021-08-20 21:21:44 +0300

    Agregar archivos de proyecto.

---
+++ DB_Project/Ventas.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace DB_Project
+{
+    partial class Ventas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbox_V_CodPro = new System.Windows.Forms.TextBox();
+            this.tbox_V_CodPie = new System.Windows.Forms.TextBox();
+            this.tbox_V_CodPj = new System.Windows.Forms.TextBox();
+            this.tbox_V_Cantidad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bt_N_modificar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bt_N_Eliminar = new System.Windows.Forms.Button();
+            this.bt_N_Buscar = new System.Windows.Forms.Button();
+            this.bt_N_insertar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cod Provider";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cod Piece";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cod project";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Quantity";
+            // 
+            // tbox_V_CodPro
+            // 
+            this.tbox_V_CodPro.Location = new System.Drawing.Point(245, 33);
+            this.tbox_V_CodPro.Name = "tbox_V_CodPro";
+            this.tbox_V_CodPro.Size = new System.Drawing.Size(100, 20);
+            this.tbox_V_CodPro.TabIndex = 7;
+            this.tbox_V_CodPro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbox_V_CodPro_KeyPress);
+            // 
+            // tbox_V_CodPie
+            // 
+            this.tbox_V_CodPie.Location = new System.Drawing.Point(245, 59);
+            this.tbox_V_CodPie.Name = "tbox_V_CodPie";
+            this.tbox_V_CodPie.Size = new System.Drawing.Size(100, 20);
+            this.tbox_V_CodPie.TabIndex = 8;
+            // 
+            // tbox_V_CodPj
+            // 
+            this.tbox_V_CodPj.Location = new System.Drawing.Point(245, 85);
+            this.tbox_V_CodPj.Name = "tbox_V_CodPj";
+            this.tbox_V_CodPj.Size = new System.Drawing.Size(100, 20);
+            this.tbox_V_CodPj.TabIndex = 9;
+            // 
+            // tbox_V_Cantidad
+            // 
+            this.tbox_V_Cantidad.Location = new System.Drawing.Point(245, 111);
+            this.tbox_V_Cantidad.Name = "tbox_V_Cantidad";
+            this.tbox_V_Cantidad.Size = new System.Drawing.Size(100, 20);
+            this.tbox_V_Cantidad.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(342, 262);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATA";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::DB_Project.Properties.Resources.PK;
+            this.pictureBox4.Location = new System.Drawing.Point(218, 47);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::DB_Project.Properties.Resources.PK;
+            this.pictureBox3.Location = new System.Drawing.Point(218, 73);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::DB_Project.Properties.Resources.PK;
+            this.pictureBox2.Location = new System.Drawing.Point(218, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bt_N_modificar
+            // 
+            this.bt_N_modificar.BackgroundImage = global::DB_Project.Properties.Resources.update;
+            this.bt_N_modificar.Location = new System.Drawing.Point(108, 212);
+            this.bt_N_modificar.Name = "bt_N_modificar";
+            this.bt_N_modificar.Size = new System.Drawing.Size(75, 46);
+            this.bt_N_modificar.TabIndex = 18;
+            this.bt_N_modificar.UseVisualStyleBackColor = true;
+            this.bt_N_modificar.Click += new System.EventHandler(this.bt_N_modificar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::DB_Project.Properties.Resources.ventas1;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 140);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bt_N_Eliminar
+            // 
+            this.bt_N_Eliminar.BackgroundImage = global::DB_Project.Properties.Resources.Delete;
+            this.bt_N_Eliminar.Location = new System.Drawing.Point(27, 212);
+            this.bt_N_Eliminar.Name = "bt_N_Eliminar";
+            this.bt_N_Eliminar.Size = new System.Drawing.Size(75, 46);
+            this.bt_N_Eliminar.TabIndex = 16;
+            this.bt_N_Eliminar.UseVisualStyleBackColor = true;
+            this.bt_N_Eliminar.Click += new System.EventHandler(this.bt_N_Eliminar_Click);
+            // 
+            // bt_N_Buscar
+            // 
+            this.bt_N_Buscar.BackColor = System.Drawing.SystemColors.Control;
+            this.bt_N_Buscar.BackgroundImage = global::DB_Project.Properties.Resources.Buscar;
+            this.bt_N_Buscar.Location = new System.Drawing.Point(270, 212);
+            this.bt_N_Buscar.Name = "bt_N_Buscar";
+            this.bt_N_Buscar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bt_N_Buscar.Size = new System.Drawing.Size(75, 46);
+            this.bt_N_Buscar.TabIndex = 15;
+            this.bt_N_Buscar.Text = "      CodPro";
+            this.bt_N_Buscar.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.bt_N_Buscar.UseVisualStyleBackColor = false;
+            this.bt_N_Buscar.Click += new System.EventHandler(this.bt_N_Buscar_Click);
+            // 
+            // bt_N_insertar
+            // 
+            this.bt_N_insertar.BackgroundImage = global::DB_Project.Properties.Resources.insertar;
+            this.bt_N_insertar.Location = new System.Drawing.Point(189, 212);
+            this.bt_N_insertar.Name = "bt_N_insertar";
+            this.bt_N_insertar.Size = new System.Drawing.Size(75, 46);
+            this.bt_N_insertar.TabIndex = 13;
+            this.bt_N_insertar.UseVisualStyleBackColor = true;
+            this.bt_N_insertar.Click += new System.EventHandler(this.bt_N_insertar_Click);
+            // 
+            // Ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 292);
+            this.Controls.Add(this.bt_N_modificar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bt_N_Eliminar);
+            this.Controls.Add(this.bt_N_Buscar);
+            this.Controls.Add(this.bt_N_insertar);
+            this.Controls.Add(this.tbox_V_Cantidad);
+            this.Controls.Add(this.tbox_V_CodPj);
+            this.Controls.Add(this.tbox_V_CodPie);
+            this.Controls.Add(this.tbox_V_CodPro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Ventas";
+            this.Text = "Sales";
+            this.Load += new System.EventHandler(this.Ventas_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbox_V_CodPro;
+        private System.Windows.Forms.TextBox tbox_V_CodPie;
+        private System.Windows.Forms.TextBox tbox_V_CodPj;
+        private System.Windows.Forms.TextBox tbox_V_Cantidad;
+        private System.Windows.Forms.Button bt_N_insertar;
+        private System.Windows.Forms.Button bt_N_Buscar;
+        private System.Windows.Forms.Button bt_N_Eliminar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bt_N_modificar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ DB_Project/Ventas.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB_Project
+{
+    
+    public partial class Ventas : Form
+    {
+        
+        Conexion conex = new Conexion();       
+
+        //interface
+        void activa_textos()    //activa los textBox para escribir
+        {
+            this.tbox_V_CodPro.Enabled = true;
+            this.tbox_V_CodPie.Enabled = true;
+            this.tbox_V_CodPj.Enabled = true;
+            this.tbox_V_Cantidad.Enabled = true;            
+        }
+        //********************************************************************************************
+        public void buscar()
+        {
+            try
+            {
+                //busca si existe                
+                string sql = string.Format("SELECT codpro FROM ventas WHERE codpro='{0}'", tbox_V_CodPro.Text);
+                Boolean resul = conex.ConsultaResul(sql);
+
+                if (resul)
+                {
+                    MessageBox.Show("PROVIDER EXIST IN THE DATABASE");                                     
+                    this.bt_N_Eliminar.Enabled = true;
+                    this.bt_N_modificar.Enabled = true;
+                    activa_textos();
+                                        
+                    SqlDataAdapter adp = new SqlDataAdapter("SELECT codpro,codpie,codpj,cantidad FROM ventas WHERE codpro='" + this.tbox_V_CodPro.Text + "'", conex.sqlConn);
+                    DataSet ds = new DataSet();
+                    adp.Fill(ds, "ventas");
+                    this.tbox_V_CodPro.Text = ds.Tables[0].Rows[0]["codpro"].ToString();
+                    this.tbox_V_CodPie.Text = ds.Tables[0].Rows[0]["codpie"].ToString();
+                    this.tbox_V_CodPj.Text = ds.Tables[0].Rows[0]["codpj"].ToString();
+                    this.tbox_V_Cantidad.Text = ds.Tables[0].Rows[0]["cantidad"].ToString();                                                         
+                    this.tbox_V_CodPie.Focus();                    
+                }
+                else
+                {
+                    MessageBox.Show("PROVIDER DOES NOT EXIST IN THE DATABASE");                    
+                    this.bt_N_insertar.Enabled = true;
+                    this.bt_N_Eliminar.Enabled = false;
+                    this.bt_N_modificar.Enabled = false;                                       
+                    activa_textos();                   
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR..." + exc);                
+            }
+        }
+        //*******************************************************************************************
+        public void insertar()
+        {
+            try
+            {
+                if (tbox_V_CodPro.Text == "" || tbox_V_CodPie.Text == "" || tbox_V_CodPj.Text == "")
+                {
+                    MessageBox.Show("PRIMARY KEYS CAN NOT STAY BLANK");
+                }
+                else
+                {                    
+                    SqlCommand cmd = new SqlCommand("INSERT INTO ventas(codpro,codpie,codpj,cantidad)VALUES('"
+                    + this.tbox_V_CodPro.Text + "','" + this.tbox_V_CodPie.Text
+                    + "','" + this.tbox_V_CodPj.Text
+                    + "','" + this.tbox_V_Cantidad.Text + "')", conex.sqlConn);
+
+                    conex.sqlConn.Open();
+                    cmd.ExecuteNonQuery();
+                    conex.sqlConn.Close();
+
+                    MessageBox.Show("ADDED SUCCESSFULLY");
+                    this.tbox_V_CodPro.Focus();                    
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("ERROR... NOT ADDED" + ee);
+                conex.sqlConn.Close();
+            }            
+        }
+        //****************************************************************************************
+        public void modificar()
+        {
+            try
+            {
+                if (tbox_V_CodPro.Text == "" || tbox_V_CodPie.Text == "" || tbox_V_CodPj.Text == "")
+                {
+                    MessageBox.Show("PRIMARY KEYS CAN NOT STAY BLANK");
+                }
+                else
+                {
+                    //busca si existe la combinacion de Primary key en la tabla ventas                
+                    string sql = string.Format("SELECT * FROM ventas WHERE ventas.codpro = '" + tbox_V_CodPro.Text + "' and ventas.codpie = '" + tbox_V_CodPie.Text + "' and ventas.codpj = '" + tbox_V_CodPj.Text + "'");
+                    Boolean resul = conex.ConsultaResul(sql);
+                    if (resul) //si existe hago esto
+                    {
+                        MessageBox.Show("PRIMARY KEYs: OK  :-D ");
+
+                        string str = "UPDATE ventas SET cantidad = '" + tbox_V_Cantidad.Text + "' where ventas.codpro = '" + tbox_V_CodPro.Text + "' and ventas.codpie = '" + tbox_V_CodPie.Text + "' and ventas.codpj = '" + tbox_V_CodPj.Text + "'";
+                        SqlCommand cmd = new SqlCommand(str, conex.sqlConn);
+                        conex.sqlConn.Open();
+                        cmd.ExecuteNonQuery();
+                        conex.sqlConn.Close();
+                        MessageBox.Show("MODIFIED SUCCESSFULLY");
+                       
+                        this.tbox_V_CodPro.Focus();
+                    }
+                    else {
+                        MessageBox.Show("ERROR... PRIMARY KEYs: NOT FOUND");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERROR... NOT MODIFIED" + e);
+                conex.sqlConn.Close();
+            }
+        }
+        //***************************************************************************************
+        public void eliminar()
+        {
+            try
+            {
+                //busca si existe la combinacion de Primary key en la tabla ventas                
+                string sql = string.Format("SELECT * FROM ventas WHERE ventas.codpro = '" + tbox_V_CodPro.Text + "' and ventas.codpie = '" + tbox_V_CodPie.Text + "' and ventas.codpj = '" + tbox_V_CodPj.Text + "'");
+                Boolean resul = conex.ConsultaResul(sql);
+
+                if (resul) //si existe hago esto
+                {
+                    string str = "DELETE FROM ventas where ventas.codpro = '" + tbox_V_CodPro.Text + "' and ventas.codpie = '" + tbox_V_CodPie.Text + "' and ventas.codpj = '" + tbox_V_CodPj.Text + "'";
+                    SqlCommand cmd = new SqlCommand(str, conex.sqlConn);
+
+                    conex.sqlConn.Open();
+                    cmd.ExecuteNonQuery();
+                    conex.sqlConn.Close();
+                    MessageBox.Show("DELETED SUCCESSFULLY");
+                }
+                else
+                {
+                    MessageBox.Show("ERROR... PRIMARY KEYs: NOT FOUND");
+                }                
+                this.tbox_V_CodPro.Focus();                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERROR... NOT DELETED" + e);
+                conex.sqlConn.Close();
+            }
+        }
+        //***************************************************************************************
+        //------------------------------------------
+        Menu mn = new Menu();
+        // Delegado
+        public delegate void Delegado_Metodo(string mensaje);    //175
+        //Evento
+        public event Delegado_Metodo MiEvento;        
+        //****************************************************************************************
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+        //****************************************************************************************
+        private void Ventas_Load(object sender, EventArgs e) { }
+        //****************************************************************************************
+        //Botones de Control
+        private void bt_N_Buscar_Click(object sender, EventArgs e)
+        {
+            buscar();
+            this.MiEvento("SELECT * FROM ventas WHERE codpro='" + this.tbox_V_CodPro.Text + "'");
+        }
+        //*********************************************************************************
+        private void bt_N_insertar_Click(object sender, EventArgs e)
+        {            
+            insertar();
+            this.MiEvento("");
+        }
+        //*********************************************************************************
+        private void bt_N_Eliminar_Click(object sender, EventArgs e)
+        {
+            eliminar();
+            this.MiEvento("");
+        }
+        //*********************************************************************************
+        private void bt_N_modificar_Click(object sender, EventArgs e)
+        {
+            modificar();
+            this.MiEvento("");
+        }        
+        //*********************************************************************************
+        private void tbox_V_CodPro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                e.Handled = true;           //eliminar el sonido al presionar la tecla
+                buscar();
+                this.MiEvento("SELECT * FROM ventas WHERE codpro='" + this.tbox_V_CodPro.Text + "'");
+            }
+            else if (e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                e.Handled = true;
+                //limpiar textBox
+                this.tbox_V_CodPro.Text = "";
+                this.tbox_V_CodPie.Text = "";
+                this.tbox_V_CodPj.Text = "";
+                this.tbox_V_Cantidad.Text = "";                
+            }
+            else
+            {
+                //lo que haria en otro caso
+            }
+        }     
+        //***************************************************************************************       
+    }
+}
